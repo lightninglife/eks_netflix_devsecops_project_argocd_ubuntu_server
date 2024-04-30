d56a946ebf3c323d7d4aa79b5d3f557f96640ee8
lightninglife <zhaofeng8711@gmail.com>
2024-04-29 20:48:41 -0400

    Update api_server_endpoint.sh

--- eks_alb/api_server_endpoint.sh
+++ eks_alb/api_server_endpoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Execute your command to retrieve api server endpoint data
-api_endpoint=$(sudo -u ubuntu /snap/bin/aws eks describe-cluster --region us-east-1 --name eks-netflix-cluster --query "cluster.endpoint" --output text)
+api_endpoint=$(sudo -u ubuntu /usr/bin/aws eks describe-cluster --region us-east-1 --name eks-netflix-cluster --query "cluster.endpoint" --output text)
 
 # Construct a JSON object with the api server endpoint data
 cat <<EOF
